f1a137e3d4afce5e4fe0cd1026330d38e9a83880
Lou Pruitt <Lpruitt2004@gmail.com>
2018-06-16 22:42:51 -0700

    completed functional components

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,40 @@
+import React from 'react';
+import Table from './table'
 
+export default () => {
 
+    const students = [
+        {
+            name: 'Michael Gary Scott',
+            course: 'Improv',
+            grade: 67
+        },
+        {
+            name: 'Dwight Schrute',
+            course: 'Business Management',
+            grade: 100
+        },
+        {
+            name: 'Jim Halpert (Big Tuna)',
+            course: 'Public Speaking',
+            grade: 75
+        },
+        {
+            name: 'Andrew Baines Bernard',
+            course: 'A capella',
+            grade: 87
+        },
+        {
+            name: 'Pamela Morgan Halpert',
+            course: 'Art Theory',
+            grade: 99
+        }
+    ];
+
+    return (
+        <div className='container'>
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+    )
+}
