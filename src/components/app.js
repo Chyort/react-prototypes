5ff7face85375bd44d2bad8e6bb24de3a7e0bebe
Lou Pruitt <Lpruitt2004@gmail.com>
2018-06-19 18:00:40 -0700

    completed some FSs

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,19 @@
+import React from 'react';
+import ContactList from './contact_list';
+import ContactForm from './contact_form'
 
+class App extendsComponent {
+    constructor(props) {
+        super(props);
+    }
+    <div className="container">
+        <h1 className="text-center">Address Book</h1>
+        <div className="row">
+            <div className="col-4">
+                <ContactForm/>
+            </div>
+            <ContactList/>
+        </div>
+    </div>
+};
 
---
+++ src/data/contacts.js
@@ -0,0 +1,25 @@
+export default [
+    {
+        firstName: 'Joe',
+        lastName: 'MacMillan',
+        phone: '123-456-7890',
+        email: 'cardiffGiant@ibm.com'
+    },
+    {
+        firstName: 'Catherine',
+        lastName: 'Howe',
+        phone: '123-456-7899',
+        email: 'cardiffBios@ibm.com'
+    },
+    {
+        firstName: 'John',
+        lastName: 'Bosworth',
+        phone: '123-456-7898',
+        email: 'cardiffManagement@ibm.com'
+    },
+    {
+        firstName: 'Gordon',
+        lastName: 'Clarke',
+        phone: '123-456-7897',
+        email: 'cardiffGiant@ibm.com'
+    } ];
