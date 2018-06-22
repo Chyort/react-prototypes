ea474ea86ae6c19bb266fd0dfb2c5e6d0b1a952a
Lou Pruitt <Lpruitt2004@gmail.com>
2018-06-22 16:10:21 -0700

    started react axios

---
+++ src/components/movie_container.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Movie from './movies';
+import axios from 'axios';
+
+
+class MovieContainer extends Component {
+
+    componentDidMount() {
+        const url = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json';
+
+        axios.get(url).then((resp) => {
+            console.log('Response:', resp);
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <h2>Movie Container</h2>
+                <Movie/>
+            </div>
+        );
+    }
+}
+
+export default MovieContainer;
