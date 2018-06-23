849e8f08361bf9ee1f9019be0e4847d430e6472d
Lou Pruitt <Lpruitt2004@gmail.com>
2018-06-22 17:44:29 -0700

    Completed Routing Intro

---
+++ src/components/gifts_parties.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            <h1>Gifts & Parties</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab corporis eum illum minus qui quibusdam, sunt? Blanditiis natus officia quaerat ullam unde. Ab laboriosam nam pariatur placeat? Deserunt, hic, iusto!</p>
+        </div>
+    )
+}
---
+++ src/components/welcome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            <h1>Welcome</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque ea enim fugiat vel vitae! A alias assumenda at aut deleniti eaque fugit iusto laudantium minus necessitatibus sint, sunt veritatis, voluptate?</p>
+        </div>
+    )
+}
